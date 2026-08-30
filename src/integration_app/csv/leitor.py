e0e2719dcc0494c4eccf6694bb4bd47ceb64f16a
Ivieri <99974078+Ivieri-Ramos@users.noreply.github.com>
2026-08-30 19:10:09 -0300

    fix: fixed incorrect parameters, logics errors, incorrect returns and more (#15)
    parser:
    - Fixed the grammar.lark file to properly parse the ^ or **.
    - Remove unused function in transformer.py.
    
    simspon.cpp:
    - Fix incorrect parameters types in integrate method.
    
    ftc:
    - Fix incorrect parameters types in integrate method.
    
    integration_app:
    - Add __init__.py for integration_app folder.
    
    csv:
    - Fix logic error and increment code safety.

--- src/integration_app/csv/leitor.py
+++ src/integration_app/csv/leitor.py
@@ -1,37 +1,37 @@
-import math
+from dataclasses import dataclass
 import csv
 
-def ler_e_verificar_dados(caminho_arquivo):
-    vetor_x = []
-    vetor_fx = []
-    
+@dataclass(frozen=True)
+class CSVDataDTO:
+    vector_x: list[float]
+    vector_y: list[float]
+
+def ler_dados_csv(caminho_arquivo) -> CSVDataDTO:
+    vetor_x, vetor_fx = [], []
+
     try:
-        arquivo = open(caminho_arquivo, 'r', encoding='utf-8')
-    except FileNotFoundError:
-        raise ValueError(f"Erro: O arquivo '{caminho_arquivo}' não foi encontrado.")
-        
-    delta_x_referencia = None
-    
-    with arquivo:
-        leitor = csv.reader(arquivo, delimiter=';')
-        for partes in leitor:
-            if len(partes) == 2:
-                try:
-                    novo_x = float(partes[0])
-                    novo_fx = float(partes[1])
-                except ValueError:
+        with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
+            leitor = csv.reader(arquivo, delimiter=';')
+
+            next(leitor, None)
+
+            for num_linha, partes in enumerate(leitor, start=2):
+                if not partes:
                     continue
-                
-                if len(vetor_x) > 0:
-                    delta_atual = novo_x - vetor_x[-1]
-                    
-                    if len(vetor_x) == 1:
-                        delta_x_referencia = delta_atual
-                    else:
-                        if not math.isclose(delta_atual, delta_x_referencia, abs_tol=1e-5):
-                            raise ValueError("Erro: impossibilidade de continuar por causa dos dados irregulares.")
-                
+
+                if len(partes) != 2:
+                    raise ValueError(f"Erro na linha {num_linha}: esperava 2 colunas, mas encontrou {len(partes)}.")
+
+                try:
+                    novo_x = float(partes[0].replace(',', '.'))
+                    novo_fx = float(partes[1].replace(',', '.'))
+                except ValueError:
+                    raise ValueError(f"Erro na linha {num_linha}: dado inválido detectado ({partes[0]}; {partes[1]}).")
+
                 vetor_x.append(novo_x)
                 vetor_fx.append(novo_fx)
-                
-    return vetor_x, vetor_fx
+
+    except FileNotFoundError:
+        raise ValueError(f"Erro: O arquivo '{caminho_arquivo}' não foi encontrado.")
+
+    return CSVDataDTO(vetor_x, vetor_fx)
